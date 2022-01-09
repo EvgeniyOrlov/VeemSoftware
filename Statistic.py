dc7066665dc56ab8af26e03bce8de48c27f08664
Evgeniy Orlov <evg,orlov94@gmai.com>
2022-01-09 19:56:01 +0300

    save format - csv

--- Statistic.py
+++ Statistic.py
@@ -5,7 +5,6 @@ import datetime
 import csv
 
 
-
 def proc(way):
     for el in psutil.pids()[1:]:
         pid = psutil.Process(el).exe()
@@ -18,22 +17,25 @@ def proc(way):
 w = input('Ведите полный путь к исполняемому процессу: ')
 n = int(input('Задайте интервал сбора статистики: '))
 
-process_for_test = subprocess.run([w], stdout=subprocess.PIPE)
+process_for_test = subprocess.Popen([w])
 print('Запуск...')
-sleep(7)
+sleep(5)
 print('Скрипт запущен.\n'
-      'Для прекращения сбора статистики закройте окно.\n'
+      'Для прекращения сбора статистики закройте окно или приложение.\n'
       '\n'
       'После прекращения работы приложения, файлы сохраняются в директорию "Statistic files"')
 now = datetime.datetime.now()
 p = psutil.Process(proc(w))
 
-with open(f'Statistic files/{p.name()} {now.strftime("%d-%m-%Y %H-%M")}.csv', 'a', newline='') as csv_file:
+with open(f'statistic files/{p.name()} {now.strftime("%d-%m-%Y %H-%M")}.csv', 'a', newline='') as csv_file:
     csv_writer = csv.writer(csv_file, delimiter=';')
-    csv_writer.writerow(['Загрузка CPU', 'Потребление памяти: Private Bytes', 'Потребление памяти: Working Set', 'Количество открытых хендлов'])
+    csv_writer.writerow(['Загрузка CPU', 'Потребление памяти: Private Bytes', 'Потребление памяти: Working Set',
+                         'Количество открытых хендлов'])
 
 while True:
     with p.oneshot():
-        with open(f'Statistic files/{p.name()} {now.strftime("%d-%m-%Y %H-%M")}.csv', 'a', encoding='UTF-8', newline='') as csv_file:
+        with open(f'statistic files/{p.name()} {now.strftime("%d-%m-%Y %H-%M")}.csv', 'a', encoding='UTF-8',
+                  newline='') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=';')
-            csv_writer.writerow([p.cpu_percent(interval=n), p.memory_info().private / 1024, p.memory_info().wset / 1024, p.num_handles()])
+            csv_writer.writerow([p.cpu_percent(interval=n), p.memory_info().private / 1024,
+                                 p.memory_info().wset / 1024, p.num_handles()])
--- statistic.spec
+++ statistic.spec
@@ -4,7 +4,7 @@
 block_cipher = None
 
 
-a = Analysis(['Statistic.py'],
+a = Analysis(['statistic.py'],
              pathex=[],
              binaries=[],
              datas=[],
@@ -24,7 +24,7 @@ exe = EXE(pyz,
           a.scripts, 
           [],
           exclude_binaries=True,
-          name='Statistic',
+          name='statistic',
           debug=False,
           bootloader_ignore_signals=False,
           strip=False,
@@ -41,4 +41,4 @@ coll = COLLECT(exe,
                strip=False,
                upx=True,
                upx_exclude=[],
-               name='Statistic')
+               name='statistic')
